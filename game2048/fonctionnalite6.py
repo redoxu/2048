4282b22e10d7cb843772903c3fa10758ba5a64bb
Ouhammou Yassir <yassir.ouhammou@student-cs.fr>
2024-11-13 21:05:18 +0100

    hojom les erreurs

--- game2048/fonctionnalite6.py
+++ game2048/fonctionnalite6.py
@@ -29,13 +29,13 @@ def game_play():
     size=ask_and_read_grid_size()
     theme=ask_and_read_grid_theme()
     grid=fonctio1.init_game(size)
-    fonctio2.grid_to_string_with_size_and_theme(grid,fonctio2.THEMES[f"{theme}"],size)
+    fonctio2.grid_to_string_with_size_and_theme(grid,fonctio2.THEMES[str(theme)],size)
     while not fonctio5.is_game_over(grid):
 #demander a lutilisateur son move... reste à faire
         move=fonctio3.read_player_command() 
         grid=fonctio4.move_grid(grid,move)
         fonctio1.grid_add_new_tile(grid)
-        fonctio2.grid_to_string_with_size_and_theme(grid,size,fonctio2.THEMES[f"{theme}"])
+        fonctio2.grid_to_string_with_size_and_theme(grid,fonctio2.THEMES[str(theme)],size)
     if fonctio5.jeu_gagnant(grid):
         return ("jeu gagnant")
     else:
