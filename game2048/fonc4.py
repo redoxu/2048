b5ee48d63d7c30bcbdafc7ffdda709b6b348aaa5
Ouchouker Tarek <tarek.ouchouker@student-cs.fr>
2024-11-13 18:06:50 +0100

    fonctionalite 4 done

--- game2048/fonc4.py
+++ game2048/fonc4.py
@@ -1,9 +1,84 @@
+def no_consective(liste):    #si il n'y a pas deux même nbr consicutifs
+    n =len(liste)
+    for j in range(n-1):
+        if liste[j] == liste[j+1]:
+            return False
+    return True
+def listesz(ligne):
+    lignesanszero=[]
+    for ele in ligne:
+        if ele != 0:
+            lignesanszero +=[ele]
+    return lignesanszero
+
 def move_row_left(ligne):
     n = len(ligne)
-    if ligne == [0]*n:
-        return ligne
-    while ligne[0] ==0:
-        for i in range(n-1):
-            ligne [i]=ligne[i+1]
-        ligne[n-1]=0
-    return ligne
+    L=[]
+    lignesanszero=listesz(ligne)
+    if no_consective(lignesanszero):
+        while len(lignesanszero)<n:
+            lignesanszero += [0]
+    for i in range(len(lignesanszero)-1):
+        if lignesanszero[i]==lignesanszero[i+1]:
+            lignesanszero[i]=lignesanszero[i]*2
+            lignesanszero[i+1]= 0
+    lignesanszero=listesz(lignesanszero)
+    while len(lignesanszero)<n:
+            lignesanszero += [0]
+    return lignesanszero
+
+
+def move_row_right(row):
+    L=row[::-1]
+    l=move_row_left(L)
+    return l[::-1]
+
+
+def transpose(liste_de_listes):
+    return [[liste_de_listes[j][i] for j in range(len(liste_de_listes))] for i in range(len(liste_de_listes[0]))]
+
+def move_grid_left(grid):
+    n = len(grid)
+    grid1=grid
+    for i in range(n):
+        grid1[i]=move_row_left(grid[i])
+    return grid1
+
+def move_grid_right(grid):
+    n = len(grid)
+    grid1=grid
+    for i in range(n):
+        grid1[i]=move_row_right(grid[i])
+    return grid1
+
+def move_grid_up(grid):
+    grid2 =grid
+    n = len(grid)
+    grid1=transpose(grid)
+    for i in range(n):
+        grid2[i]=move_row_left(grid1[i])
+    return transpose(grid2)
+
+def move_grid_down(grid):
+    grid2 =grid
+    n = len(grid)
+    grid1=transpose(grid)
+    for i in range(n):
+        grid2[i]=move_row_right(grid1[i])
+    return transpose(grid2)
+'''
+def move_grid_down(grid):
+    l=grid[::-1]
+    L=move_grid_up(grid)
+    return L[::-1]
+'''
+def move_grid(grid,d):
+    if d=="left":
+        return move_grid_left(grid)
+    elif d =="right":
+        return move_grid_right(grid)
+    elif d == "up":
+        return move_grid_up(grid)
+    elif d == "down":
+        return move_grid_down(grid)
+    
--- test_fonctionalite4.py
+++ test_fonctionalite4.py
@@ -1,4 +1,4 @@
-from game2048.grid_2048 import *
+from game2048.fonc4 import *
 from pytest import *
 
 
@@ -13,3 +13,24 @@ def test_move_row_left():
     assert move_row_left([2, 4, 2, 2]) == [2, 4, 4, 0]
     assert move_row_left([2, 4, 4, 0]) == [2, 8, 0, 0]
     assert move_row_left([4, 8, 16, 32]) == [4, 8, 16, 32]
+
+
+def test_move_row_right():
+
+    assert move_row_right([2, 0, 0, 0]) == [0, 0, 0, 2]
+    assert move_row_right([0, 2, 0, 4]) == [0, 0, 2, 4]
+    assert move_row_right([2, 2, 0, 4]) == [0, 0, 4, 4]
+    assert move_row_right([2, 2, 2, 2]) == [0, 0, 4, 4]
+    assert move_row_right([4, 2, 0, 2]) == [0, 0, 4, 4]
+    assert move_row_right([2, 0, 0, 2]) == [0, 0, 0, 4]
+    assert move_row_right([2, 4, 2, 2]) == [0, 2, 4, 4]
+    assert move_row_right([2, 4, 4, 0]) == [0, 0, 2, 8]
+    assert move_row_right([4, 8, 16, 32]) == [4, 8, 16, 32]
+
+
+
+def test_move_grid():
+    assert move_grid([[2,0,0,2], [4, 4, 0, 0], [8, 0, 8, 0], [0, 2, 2, 0]],"left") == [[4,0,0,0], [8, 0, 0, 0], [16, 0, 0, 0], [4, 0, 0, 0]]
+    assert move_grid([[2,0,0,2], [4, 4, 0, 0], [8, 0, 8, 0], [0, 2, 2, 0]],"right") == [[0,0,0,4], [0, 0, 0, 8], [0, 0, 0, 16], [0, 0, 0, 4]]
+    assert move_grid([[2,0,0,2], [2, 4, 0, 0], [8, 4, 2, 0], [8, 2, 2, 0]],"up") == [[4,8,4,2], [16, 2, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    assert move_grid([[2,0,0,2], [2, 4, 0, 0], [8, 4, 2, 0], [8, 2, 2, 0]],"down") == [[0, 0, 0, 0], [0, 0, 0, 0],[4,8,0,0],[16, 2, 4, 2]]
